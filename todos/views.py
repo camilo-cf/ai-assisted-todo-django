6ecf8c197130c0f3ab34c74efcc642b5f132923a
Camilo A. Cáceres <32584840+camilo-cf@users.noreply.github.com>
2025-11-23 20:55:01 -0500

    Improve UI and add success messages

--- todos/views.py
+++ todos/views.py
@@ -1,3 +1,5 @@
+from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib import messages
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from .models import Todo
@@ -10,17 +12,23 @@ class TodoListView(ListView):
 
     ordering = ["-created_at"]
 
-class TodoCreateView(CreateView):
+class TodoCreateView(SuccessMessageMixin, CreateView):
     model = Todo
     fields = ["title", "description", "due_date", "is_resolved"]
     success_url = reverse_lazy("todo-list")
+    success_message = "Todo '%(title)s' was created successfully."
 
-class TodoUpdateView(UpdateView):
+class TodoUpdateView(SuccessMessageMixin, UpdateView):
     model = Todo
     fields = ["title", "description", "due_date", "is_resolved"]
     success_url = reverse_lazy("todo-list")
+    success_message = "Todo '%(title)s' was updated successfully."
 
 class TodoDeleteView(DeleteView):
     model = Todo
     success_url = reverse_lazy("todo-list")
+    success_message = "Todo was deleted successfully."
 
+    def delete(self, request, *args, **kwargs):
+        messages.success(self.request, self.success_message)
+        return super().delete(request, *args, **kwargs)
